a3aa58458dbbae36310737a4ef910875c32aac63
arifullov <arifullov73@icloud.com>
2024-05-10 20:02:44 +0400

    2nd add actions with db

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -6,32 +6,90 @@ import (
 	"log"
 	"net"
 
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	desc "github.com/arifullov/chat-server/pkg/chat_v1"
 )
 
 const grpcPort = 50053
+const dbDSN = "host=localhost port=5002 dbname=chat_db user=chat password=secret_pass sslmode=disable"
 
 type server struct {
 	desc.UnimplementedChatV1Server
+	dbPool *pgxpool.Pool
 }
 
 func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
 	log.Printf("create chat: %v", req)
+	builderInsert := sq.Insert("chats").
+		PlaceholderFormat(sq.Dollar).
+		Columns("usernames").
+		Values(req.Usernames).
+		Suffix("RETURNING id")
+	query, args, err := builderInsert.ToSql()
+	if err != nil {
+		log.Printf("failed to build query: %v", err)
+		return nil, status.Errorf(codes.Unavailable, "failed to build query")
+	}
+
+	var chatID int64
+	err = s.dbPool.QueryRow(ctx, query, args...).Scan(&chatID)
+	if err != nil {
+		log.Printf("failed to insert chat: %v", err)
+		return nil, status.Errorf(codes.Unavailable, "failed to insert chat")
+	}
 	return &desc.CreateResponse{
-		Id: 228,
+		Id: chatID,
 	}, nil
 }
 
 func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	builderDelete := sq.Delete("chats").
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{"id": req.Id})
+
+	query, args, err := builderDelete.ToSql()
+	if err != nil {
+		log.Printf("failed to build query: %v", err)
+		return nil, status.Errorf(codes.Unavailable, "failed to delete chat")
+	}
+
+	res, err := s.dbPool.Exec(ctx, query, args...)
+	if err != nil {
+		log.Printf("failed to delete chat: %v", err)
+		return nil, status.Errorf(codes.Unavailable, "failed to delete chat")
+	}
+	if res.RowsAffected() == 0 {
+		return nil, status.Errorf(codes.NotFound, "chat not found")
+	}
 	log.Printf("delete chat: %v", req)
 	return nil, nil
 }
 func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
 	log.Printf("send message: %v", req)
+	builderInsert := sq.Insert("messages").
+		PlaceholderFormat(sq.Dollar).
+		Columns("\"from\"", "text", "chat_id", "timestamp").
+		Values(req.From, req.Text, req.ChatID, req.Timestamp.AsTime()).
+		Suffix("RETURNING id")
+	query, args, err := builderInsert.ToSql()
+	if err != nil {
+		log.Printf("failed to build query: %v", err)
+		return nil, status.Errorf(codes.Unavailable, "failed to build query")
+	}
+
+	var chatID int64
+	err = s.dbPool.QueryRow(ctx, query, args...).Scan(&chatID)
+	if err != nil {
+		log.Printf("failed to insert messages: %v", err)
+		return nil, status.Errorf(codes.Unavailable, "failed to insert messages")
+	}
 	return nil, nil
 }
 
@@ -40,10 +98,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	ctx := context.Background()
+	dbPool, err := pgxpool.New(ctx, dbDSN)
+	if err != nil {
+		log.Fatalf("failed db connect: %v", err)
+	}
+	if err = dbPool.Ping(ctx); err != nil {
+		log.Fatalf("failed db connect: %v", err)
+	}
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterChatV1Server(s, &server{})
+	desc.RegisterChatV1Server(s, &server{dbPool: dbPool})
 
 	log.Printf("server listening at %v", lis.Addr())
 
--- go.mod
+++ go.mod
@@ -3,12 +3,21 @@ module github.com/arifullov/chat-server
 go 1.21.5
 
 require (
+	github.com/jackc/pgx/v5 v5.5.5
 	google.golang.org/grpc v1.63.2
 	google.golang.org/protobuf v1.34.1
 )
 
 require (
+	github.com/Masterminds/squirrel v1.5.4 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
